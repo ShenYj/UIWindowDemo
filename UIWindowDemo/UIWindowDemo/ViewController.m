ea61985c0e4c3c7d6b879917fd82c9d7b89968de
ShenYj <shenyanjie123@foxmail.com>
2020-08-13 22:55:29 +0800

    ..

--- UIWindowDemo/UIWindowDemo/ViewController.m
+++ UIWindowDemo/UIWindowDemo/ViewController.m
@@ -11,7 +11,15 @@
 #import "MyWindow1.h"
 #import "ShowWindow.h"
 
-@interface ViewController ()
+static NSString * const kDemosReusedIdentifier = @"kDemosReusedIdentifier";
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+
+@property (weak, nonatomic) IBOutlet UITableView *demosTableView;
+
+@property (nonatomic, strong) NSArray <NSDictionary *> *dataSources;
+
+
 @property(nonatomic,strong)UIWindow *myWindow1;
 @property(nonatomic,strong)UIWindow *myWindow2;
 @property(nonatomic,strong)MyWindow1 *myWindow;
@@ -22,24 +30,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+    [self.demosTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kDemosReusedIdentifier];
     
     // 测试按钮
-    UIButton *tempBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    [tempBtn setTitle:@"先点我" forState:UIControlStateNormal];
-    tempBtn.frame = CGRectMake(100, 100, 100, 100);
-    tempBtn.backgroundColor = [UIColor cyanColor];
-    [tempBtn addTarget:self action:@selector(clickBtn:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:tempBtn];
-    
-    
+//    UIButton *tempBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+//    [tempBtn setTitle:@"先点我" forState:UIControlStateNormal];
+//    tempBtn.frame = CGRectMake(100, 100, 100, 100);
+//    tempBtn.backgroundColor = [UIColor cyanColor];
+//    [tempBtn addTarget:self action:@selector(clickBtn:) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:tempBtn];
+
+
     // 这个是配合着test6来测试的，查看windows数组的情况
-    UIButton *myTempBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    [myTempBtn setTitle:@"再点我，测试test2" forState:UIControlStateNormal];
-    myTempBtn.frame = CGRectMake(100, 300, 300, 100);
-    myTempBtn.backgroundColor = [UIColor greenColor];
-    [myTempBtn addTarget:self action:@selector(clickMyTempBtn:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:myTempBtn];
+//    UIButton *myTempBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+//    [myTempBtn setTitle:@"再点我，测试test2" forState:UIControlStateNormal];
+//    myTempBtn.frame = CGRectMake(100, 300, 300, 100);
+//    myTempBtn.backgroundColor = [UIColor greenColor];
+//    [myTempBtn addTarget:self action:@selector(clickMyTempBtn:) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:myTempBtn];
     
 }
 
@@ -49,49 +57,36 @@
 }
 
 
-- (void)clickBtn:(UIButton *)sender
-{
-    
-    AppDelegate *app = (AppDelegate *)[UIApplication sharedApplication].delegate;
-    app.window.alpha = 0.3;
-    
-    // 可以显示创建的window
+//- (void)clickBtn:(UIButton *)sender
+//{
+//
+//    AppDelegate *app = (AppDelegate *)[UIApplication sharedApplication].delegate;
+//    app.window.alpha = 0.3;
+//
+//    // 不能正常显示创建的window
 //    [self test1];
-    
-    // 不能正常显示创建的window
-//    [self test2];
-    
-    // 不能正常的显示创建的window
-//    [self test3];
-    
-    // 可以正常的显示创建的window
-//    [self test4];
-    
-    // 可以正常显示创建的window
-//    [self test5];
-    
-    
-//    NSLog(@"2当前所有的window %@",[UIApplication sharedApplication].windows);
-    
-    
-    
-    // 测试创建的window和[UIApplication sharedApplication].windows的关系,和上面的测试没有关系，上面代码需要全部注释掉
-    [self test6];
-    
-}
-
-// 将window设置成全局变量
-- (void)test1
-{
-    self.myWindow1 = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.myWindow1.backgroundColor = [UIColor redColor];
-    self.myWindow1.windowLevel = -1;
-    self.myWindow1.hidden = NO;
-
-}
+//
+//    // 不能正常的显示创建的window
+////    [self test2];
+//
+//    // 可以显示创建的window
+////    [self test3];
+//
+//    // 可以正常的显示创建的window
+////    [self test4];
+//
+//    // 可以正常显示创建的window
+////    [self test5];
+//
+//
+////    NSLog(@"2当前所有的window %@",[UIApplication sharedApplication].windows);
+//    // 测试创建的window和[UIApplication sharedApplication].windows的关系,和上面的测试没有关系，上面代码需要全部注释掉
+////    [self test6];
+//
+//}
 
 // 不设置成全局变量
-- (void)test2
+- (void)test1
 {
     UIWindow *myWindow3 = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     myWindow3.backgroundColor = [UIColor greenColor];
@@ -100,12 +95,13 @@
     [myWindow3 makeKeyWindow];
     
     NSLog(@"1当前所有的window %@",[UIApplication sharedApplication].windows);
-
 }
-
 // 单独创建UIWindow类
-- (void)test3
+- (void)test2
 {
+//    AppDelegate *app = (AppDelegate *)[UIApplication sharedApplication].delegate;
+//    app.window.alpha = 0.3;
+    
     MyWindow1 *myWindow = [[MyWindow1 alloc] initWithFrame:[UIScreen mainScreen].bounds];
     myWindow.backgroundColor = [UIColor greenColor];
     myWindow.windowLevel = 100;
@@ -113,23 +109,50 @@
     [myWindow makeKeyWindow];
     
 }
+// 将window设置成全局变量
+- (void)test3
+{
+    self.myWindow1 = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.myWindow1.backgroundColor = [UIColor redColor];
+    self.myWindow1.windowLevel = -1;
+    self.myWindow1.hidden = NO;
+    
+    // dismiss
+    UIButton *tempBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [tempBtn setTitle:@"Dismiss" forState:UIControlStateNormal];
+    tempBtn.bounds = CGRectMake(0, 0, 120, 50);
+    tempBtn.center = self.myWindow1.center;
+    tempBtn.backgroundColor = [UIColor cyanColor];
+    [tempBtn addTarget:self action:@selector(targetForDismissGlobalWindow) forControlEvents:UIControlEventTouchUpInside];
+    [self.myWindow1 addSubview:tempBtn];
+}
+
+
 // 单独创建UIWindow类，然后设置成全局变量
 - (void)test4
 {
     self.myWindow = [[MyWindow1 alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.myWindow.backgroundColor = [UIColor greenColor];
-    self.myWindow.windowLevel = 100;
+//    self.myWindow.windowLevel = 100;
     self.myWindow.hidden = NO;
-    [self.myWindow makeKeyWindow];
+//    [self.myWindow makeKeyWindow];
+    
+    
+    // dismiss
+    UIButton *tempBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [tempBtn setTitle:@"Dismiss" forState:UIControlStateNormal];
+    tempBtn.bounds = CGRectMake(0, 0, 120, 50);
+    tempBtn.center = self.myWindow.center;
+    tempBtn.backgroundColor = [UIColor cyanColor];
+    [tempBtn addTarget:self action:@selector(targetForDismissGlobalWindow) forControlEvents:UIControlEventTouchUpInside];
+    [self.myWindow addSubview:tempBtn];
 }
 
 // 创建window的单例进行实验
 - (void)test5
 {
     ShowWindow *window = [ShowWindow shareShowWindow];
-    
     [window show];
-
 }
 
 // 测试创建的window和[UIApplication sharedApplication].windows的关系
@@ -146,10 +169,88 @@
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark -
+
+- (void)targetForDismissGlobalWindow
+{
+    AppDelegate *app = (AppDelegate *)[UIApplication sharedApplication].delegate;
+    NSLog(@"%s", __func__);
+    if (self.myWindow1.isFocused) {
+        NSLog(@"myWindow1   isFocused");
+        self.myWindow1.hidden = YES;
+    }
+    if (self.myWindow2.isFocused) {
+        NSLog(@"myWindow2   isFocused");
+        self.myWindow2.hidden = YES;
+    }
+    if (self.myWindow.isFocused) {
+        NSLog(@"myWindow   isFocused");
+        self.myWindow.hidden = YES;
+//        self.myWindow = nil;
+    }
+    if ([ShowWindow shareShowWindow].isFocused) {
+        NSLog(@"[ShowWindow shareShowWindow]   isFocused");
+        [ShowWindow shareShowWindow].hidden = YES;
+    }
+    
+    [app.window makeKeyAndVisible];
 }
 
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataSources.count;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kDemosReusedIdentifier forIndexPath:indexPath];
+    cell.textLabel.text = self.dataSources[indexPath.row][@"title"];
+    return cell;
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    SEL selector = NSSelectorFromString(self.dataSources[indexPath.row][@"targetName"]);
+    if ([self respondsToSelector:selector]) {
+        [self performSelector:selector];
+    }
+}
+
+
+#pragma mark - lazy
+
+- (NSArray<NSDictionary *> *)dataSources {
+    if (!_dataSources) {
+        _dataSources =
+        @[
+            @{
+                @"title": @"[x] 创建UIWindow但不设置成全局变量",
+                @"targetName": NSStringFromSelector(@selector(test1))
+            },
+            @{
+                @"title": @"[x] 自定义UIWindow类但不设置成全局变量",
+                @"targetName": NSStringFromSelector(@selector(test2))
+            },
+            @{
+                @"title": @"[x] 设置成全局变量, windowLevel低于当前window",
+                @"targetName": NSStringFromSelector(@selector(test3))
+            },
+            @{
+                @"title": @"[✔️] 创建UIWindow并设置成全局变量",
+                @"targetName": NSStringFromSelector(@selector(test4))
+            },
+            @{
+                @"title": @"[✔️] UIWindow单例",
+                @"targetName": NSStringFromSelector(@selector(test5))
+            },
+        ];
+    }
+    return _dataSources;
+}
 
 @end
